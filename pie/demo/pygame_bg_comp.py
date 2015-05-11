a9ec30f7da67f1ce508fdcbc8e6d37036b52aae9
adoc <adoc@code.webmob.net>
2015-05-11 00:35:02 -0700

    try some background compositions and testing performance.

---
+++ pie/demo/pygame_bg_comp.py
@@ -0,0 +1,49 @@
+import time
+import numpy
+import pygame
+from pie.math import sub_size
+
+
+if __name__ == "__main__":
+    pygame.init()
+    pygame.surfarray.use_arraytype('numpy')
+
+    def dodge(front, back):
+        # The formula comes from http://www.adobe.com/devnet/pdf/pdfs/blend_modes.pdf
+        result=back*256.0/(256.0-front)
+        result[result>255]=255
+        result[front==255]=255
+        return result.astype('uint8')
+
+    info = pygame.display.Info()
+
+    screen = pygame.display.set_mode((512, 512))
+    clock = pygame.time.Clock()
+
+    bg1 = pygame.Surface((512,512))
+    bg1.fill((0,0,0))
+
+    i = pygame.image.load("assets/composite/bg1.png").convert()
+    bg1.blit(i, (0,0))
+
+    bg2 = pygame.image.load("assets/composite/bg2_cloud_mult.png").convert()
+
+    bg1_a = pygame.surfarray.array3d(bg1)
+    bg2_a = pygame.surfarray.array3d(bg2)
+
+    running = True
+    while running:
+        clock.tick(60)
+        print(clock.get_fps())
+
+        #screen.fill((0,0,0))
+
+        pygame.surfarray.blit_array(screen, dodge(bg2_a, bg1_a))
+
+        pygame.display.flip()
+
+        if pygame.event.get(pygame.QUIT):
+            running = False
+        pygame.event.clear()
+
+
